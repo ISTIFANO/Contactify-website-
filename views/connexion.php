a4d3b2df1940f62d213d0769aeb0d5ef67bbe9e7
istifano <elamiriamir7@gmail.com>
2024-12-24 16:13:11 +0100

    j ai ajouter la connexion with pdo

--- views/connexion.php
+++ views/connexion.php
@@ -1,2 +1,26 @@
 <?php 
+  $root = $_SERVER['DOCUMENT_ROOT'];
+  $envFilepath = "$root/Contactify-website/.env";
+
+//   echo  $envFilepath;
+
+   $env = parse_ini_file($envFilepath);
+// print_r($env); 
+   $db = $env["DATABASE"];
+   $hostname = $env["HOSTNAME"];
+   $password = $env["PASSWORD"];
+   $user = $env["USER"];
+
+   $dsn = "mysql:host=$hostname;dbname=$db;charset=UTF8";
+   
+   try {
+       $pdo = new PDO($dsn, $user, $password);
+   
+       if ($pdo) {
+           echo "Connected to the $db database successfully!";
+       }
+   } catch (PDOException $e) {
+       echo $e->getMessage();
+   }
+
 ?>
