5822a70cef2cee2acdb7095157a44b6b0e7f9d25
istifano <elamiriamir7@gmail.com>
2024-12-24 22:02:27 +0100

    j ai ajouter autoload pour donner l acces a  avariables dans env file

--- views/connexion.php
+++ views/connexion.php
@@ -1,63 +1,43 @@
-<?php 
-//   $root = $_SERVER['DOCUMENT_ROOT'];
-// //   echo  $root;
+<?php
 
-//   $envFilepath = "$root/Contactify-website/.env";
+require_once '../vendor/autoload.php'; 
 
-// //   echo  $envFilepath;
+use Dotenv\Dotenv;
 
-//    $env = parse_ini_file($envFilepath);
-// //  print_r($env); 
-//    $db = $env["DATABASE"];
-//    $hostname = $env["HOSTNAME"];
-//    $password = $env["PASSWORD"];
-//    $user = $env["USER"];
-
-//    $info = "mysql:host=$hostname;dbname=$db;charset=UTF8";
-   
-//    try {
-//        $pdo = new PDO($info, $user, $password);
-   
-//        if ($pdo) {
-//            echo "<h1>Connected to the  $db  database successfully!</h1>";
-//        }
-//    } catch (PDOException $e) {
-//        echo $e->getMessage();
-//    }
-
-class DatabaseConfiguration{
-
-    private $hostname="localhost";
-    private $database="Contactify";
-    private $user="root";
-    private $password="";
+class DatabaseConfiguration
+{
 
     protected $connexion;
+    private $hostname;
+    private $database;
+    private $user;
+    private $password;
 
     public function __construct()
     {
-
+        $dotenv = Dotenv::createImmutable(__DIR__ . '/../');
+        $dotenv->load();
+     $this->hostname = $_ENV['HOSTNAME'];
+        $this->database = $_ENV['DATABASE'];
+        $this->user = $_ENV['USER'];
+        $this->password = $_ENV['PASSWORD'];
+    }
+    public function connect()
+    {
         if (!isset($this->connexion)) {
             $info = "mysql:host=$this->hostname;dbname=$this->database;charset=UTF8";
-   
+
             try {
-                $pdo = new PDO($info, $this->user, $this->password);
-            
-             if ($pdo) {
-                    echo "<h1>Connected to the  $this->database  database successfully!</h1>";
+                $this->connexion = new PDO($info, $this->user, $this->password);
+                if ($this->connexion) {
+                    echo "<h1>Connected to the $this->database database successfully!</h1>";
                 }
             } catch (PDOException $e) {
                 echo $e->getMessage();
             }
         }
-
-return $this->connexion;
+        return $this->connexion;
     }
-
-
-
 }
 $test = new DatabaseConfiguration();
-
-// echo$test;
-?>
+$test->connect();
